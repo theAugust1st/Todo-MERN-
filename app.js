81596105b5e34d4967c0adfae8308a321f155f42
theAugust1st <bhote666@gmail.com>
2024-06-27 22:49:03 +0545

    access with the whole user with userRoute

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const User = require('./src/models/userModel');
 const app = express();
 const userRouter = require('./src/routes/userRouters');
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.post('/api/v1/userRegister',userRouter);
+app.use(userRouter);
 module.exports = {app};
