0217698cf6d91879f60e954dd6fa65cc0adce827
theAugust1st <bhote666@gmail.com>
2024-06-30 23:21:26 +0545

    sample to work with backend is ready

--- frontend/src/Components/Todo.jsx
+++ frontend/src/Components/Todo.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
-
+const tasks = []; // get title , description and time(if specified) from the backend or the user
 const Todo = () => {
   return (
-    <div className='py-8 px-2'>
+    <div className='py-8 px-2 bg-white'>
       <div>
-      <h2 className='text-white text-2xl font-semibold p-2'>To-Do List</h2>
+      <h2 className='text-purple-500 text-2xl font-semibold p-2'>To-Do List</h2>
       </div>
-      <div className='bg-white p-2 flex justify-between gap-2 rounded-md border-none '>
-      <input type='text' placeholder='Add a new task' className=' border-none outline-none bg-transparent' />
-      <input type="time" id="timepicker" className="form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"/>
-      <button className='bg-purple-500 text-white py-2 px-9 rounded-md'>Add</button>
+      <div className='form-input bg-white p-2 flex justify-between gap-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent '>
+      <input type='text' placeholder='Add a new task' className=' border-none outline-none bg-transparent grow' />
+      <div className='flex gap-2'>
+
+      <input type="time" id="timepicker" className="form-input px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent"/>
+      <button className='bg-purple-500 text-white py-2 px-9 rounded-md sm:px-6 md:px-9'>Add</button>
+      </div>
+      </div>
+      <div className='w-full h-96 overflow-y-auto '>
+        <div className='flex items-center p-2 border-b'>
+                    <input type='checkbox' className='mr-2' />
+                    <div className='flex-1'>
+                      <h3 className='text-blue-800 font-semibold'>Do this</h3>
+                      <p className='text-gray-600'>12:00</p>
+                    </div>
+                    <button className='text-red-500 hover:text-red-700'>Delete</button>
+                  </div>
       </div>
     </div>
   )
--- frontend/src/Page/Home.jsx
+++ frontend/src/Page/Home.jsx
@@ -3,7 +3,7 @@ import DetailsPartion from "../Components/DetailsPartion.jsx";
 import Todo from "../Components/Todo.jsx";
 const Home = () => {
   return (
-    <div className="bg-purple-500 w-1/2 mx-auto grid grid-cols-3 gap-5">
+    <div className="bg-purple-500 w-3/ mx-auto grid grid-cols-3 gap-5">
       <div>
         <DetailsPartion />
       </div>
