889c33360767bacfea36dc017a3c139070e4d8ed
theAugust1st <bhote666@gmail.com>
2024-07-03 22:34:04 +0545

    add ApiCustomResponse

---
+++ src/utils/ApiCustomeResponse.js
@@ -0,0 +1,8 @@
+class ApiCustomResponse {
+    constructor(statusCode, data , message="success"){
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+    }
+}
+module.exports = ApiCustomResponse;
